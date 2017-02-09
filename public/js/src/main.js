21c80a9ff5b757f282bbf7aefd5d7daee8e5982e
Will <wmaillard@gmail.com>
2017-02-08 19:38:25 -0800

    ui, says feeding

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,10 +1,18 @@
 $(function(){
+	$('#feeding').toggle();
 	$('#alert').click(function(){
 		$('body').empty().append('<div class="row"><div class="col-xs-10 offset-xs-1"><div class="spinner"><div class="cube1"></div><div class="cube2"></div></div></div></div>')
 		window.location = window.location.href + '/tutorial';
 	})
 	$('#feed').click(function(){
-		$.get("http://66.172.113.26:5000/")
+		$.get("http://66.172.113.26:5000/");
+		$('#feeding').toggle();
+		$('#feed').toggle();
+		setTimeout(function(){
+			$('#feeding').toggle();
+			$('#feed').toggle();
+		}, 3000)
+
 	})
 	
 });
